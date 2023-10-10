517f842a6036c962045fb46eb6f3fad08f3b8e10
PYKAMIA <30779727+shinakami@users.noreply.github.com>
2023-10-10 13:32:35 +0800

    Update Shit.py

--- Shit.py
+++ Shit.py
@@ -5,11 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.DataFrame(np.random.randn(100, 5), columns=list('ABCDE'))
-df = df.cumsum() # Return cumulative sum over a DataFrame or Series axis
-df.plot()
-print(df)
-plt.show()
 
 
 width_factor = 0.095
